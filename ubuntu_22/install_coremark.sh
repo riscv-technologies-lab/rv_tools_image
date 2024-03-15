162ca6e5f9a517d1571712cba7e01107afb79651
cesarus777 <ulius.cesarus@gmail.com>
2024-03-16 02:36:18 +0300

    Add coremark config for gem5

--- ubuntu_22/install_coremark.sh
+++ ubuntu_22/install_coremark.sh
@@ -15,3 +15,7 @@ set +o nounset
 source "${SCDT_INSTALLATION_ROOT}"/env.sh
 set -o nounset
 make CC=riscv64-unknown-linux-gnu-gcc XCFLAGS=-static ITERATIONS=1 link
+
+# Add gem5 config for running coremark
+cp /opt/gem5/configs/learning_gem5/part1/simple-riscv.py /opt/gem5/configs/riscv-coremark.py
+sed -z -i "s|os.path.join(\n    thispath,\n    \"../../../\",\n    \"tests/test-progs/hello/bin/riscv/linux/hello\",\n)|\"${COREMARK_DIR}/coremark.exe\"|g" /opt/gem5/configs/riscv-coremark.py
